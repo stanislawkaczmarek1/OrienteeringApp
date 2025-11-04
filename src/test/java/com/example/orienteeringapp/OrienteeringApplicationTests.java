768ef5c6882fba0bca6acb1a4848c4b80b360880
Stanislaw Kaczmarek <stkaczmarek1@wp.pl>
2025-11-04 17:07:48 +0100

    init

---
+++ src/test/java/com/example/orienteeringapp/OrienteeringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.orienteeringapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrienteeringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
